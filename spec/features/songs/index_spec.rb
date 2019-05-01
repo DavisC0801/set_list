8939e18819b7a8d4a459ab34db9ddb34f54ef391
davisc0801 <davisc1014@gmail.com>
2019-05-01 11:22:07 -0600

    Refactor index test to reflect relational databases

--- spec/features/songs/index_spec.rb
+++ spec/features/songs/index_spec.rb
@@ -1,15 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "songs index page", type: :feature do
-  it "user can see all songs" do
-    song_1 = Song.create(title: "Don't Stop Belivin'", length: 303, play_count: 123456)
-    song_2 = Song.create(title: "Never Gonna Give You Up", length: 253, play_count: 987654321)
+  before :each do
+    test = Artist.create(name: "tests")
+    @song_1 = test.songs.create(title: "Don't Stop Belivin'", length: 303, play_count: 123456)
+    @song_2 = test.songs.create(title: "Never Gonna Give You Up", length: 253, play_count: 987654321)
 
+  end
+
+  it "user can see all songs" do
     visit "/songs"
 
-    expect(page).to have_content(song_1.title)
-    expect(page).to have_content("Play Count: #{song_1.play_count}")
-    expect(page).to have_content(song_2.title)
-    expect(page).to have_content("Play Count: #{song_2.play_count}")
+    expect(page).to have_content(@song_1.title)
+    expect(page).to have_content("Play Count: #{@song_1.play_count}")
+    expect(page).to have_content(@song_2.title)
+    expect(page).to have_content("Play Count: #{@song_2.play_count}")
   end
 end
