8a0131b8d659dcfc58ddeb2f647f467bacb9a484
davisc0801 <davisc1014@gmail.com>
2019-05-02 10:06:17 -0600

    Add test for new artist creation

---
+++ spec/features/artists/new_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "when I visit the new artist form page" do
+    it "I can create a new artist" do
+      visit "/artists/new"
+
+      fill_in "Name", with: "Pat the Bunny"
+      click_on 'Create Artist'
+
+      expect(current/path).to eq("/artists/#{Artist.last.id}")
+    end
+  end
+end
+
+# As a visitor
+# When I visit '/artists/new'
+# and I fill in the artist's name
+# Then I click 'Create Artist'
+# I am redirected to this new artists show page
