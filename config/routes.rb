b5d2b376dd5ee5b2499fc8bf4506ea55123da5b1
davisc0801 <davisc1014@gmail.com>
2019-05-02 10:07:16 -0600

    Add route for new artist creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     get '/songs', to: 'songs#index'
     get '/songs/:id', to: 'songs#show'
+
+    get 'artists/new', to: 'artists#new'
   end
 end
