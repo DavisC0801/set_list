6dbceefc9bb34706e15d01796a8b48e17b9b8939
davisc0801 <davisc1014@gmail.com>
2019-05-07 10:30:39 -0600

    Add spec for song relations

--- spec/models/songs/song_spec.rb
+++ spec/models/songs/song_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 describe Song, type: :model do
   describe "relationships" do
     it {should belong_to :artist}
+    it {should have_many :playlist_songs}
+    it {should have_many(:playlists).through(:playlist_songs)}
   end
 
   describe "class_methods" do
