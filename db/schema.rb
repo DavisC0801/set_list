36e52b763dc7a386f21e9ced0357b0184cf8bc98
davisc0801 <davisc1014@gmail.com>
2019-05-07 10:18:10 -0600

    Add migration for playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190501164641) do
+ActiveRecord::Schema.define(version: 20190507161736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20190501164641) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
