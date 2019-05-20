1f7be0a5669d53594f2fd71fa03911b45be402f0
davisc0801 <davisc1014@gmail.com>
2019-05-20 10:40:53 -0600

    Rename user controller and fix typos in tests

--- spec/features/root/root_spec.rb
+++ spec/features/root/root_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe "As a visitor" do
 
       expect(current_path).to eq(new_user_path)
 
-      fill_in "User Name", with: "funbucket13"
+      fill_in "Username", with: "funbucket13"
       fill_in "Password", with: "test"
 
       click_on "Create User"
 
       new_user = User.last
 
-      expect(page).to have_content("Welcome #{new_user.user_name}!")
+      expect(page).to have_content("Welcome #{new_user.username}!")
     end
   end
 end
---
+++ spec/models/users/user_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:username)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_presence_of(:password)}
+  end
+end
