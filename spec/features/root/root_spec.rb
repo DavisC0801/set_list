7fc7dc6a99312ff57abe903bf931fd8dbe0662e0
davisc0801 <davisc1014@gmail.com>
2019-05-20 08:54:05 -0600

    Add test for user registration from root path

---
+++ spec/features/root/root_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "As a visitor" do
+  describe "when I visit '/'" do
+    it "creates new user" do
+      visit root_path
+
+      click_on "Register as User"
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in "User Name", with: "funbucket13"
+      fill_in "Password", with: "test"
+
+      click_on "Create User"
+
+      new_user = User.last
+
+      expect(page).to have_content("Welcome #{new_user.user_name}!")
+    end
+  end
+end
