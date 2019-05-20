ddb7e16de98155f918e995a2a6799c083206fba2
davisc0801 <davisc1014@gmail.com>
2019-05-20 11:14:47 -0600

    Update user controller for creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
-    
+    user = User.new(new_params)
+      if user.save
+        flash[:message] = "Welcome #{user.username}!"
+        redirect_to(songs_path)
+      else
+        render :new
+      end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def new_params
+    params.require(:user).permit(:username, :password)
   end
 end
