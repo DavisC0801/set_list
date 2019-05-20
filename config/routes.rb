544d79213dab5aa78e6a7af97cc3376d177ea11f
davisc0801 <davisc1014@gmail.com>
2019-05-20 08:58:20 -0600

    Add controller and view skeletons

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #ways to add a landing page, as syntax adds a route helper
   root to: "welcome#index"
-  get "/", to: "weclome#index", as: "welcome"
+  # get "/", to: "weclome#index", as: "welcome"
 
   resources :artists, only: [:new, :create, :show] do
     resources :songs, only: [:new, :create]
