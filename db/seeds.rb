ea8075597fb0a027f046b3e71b3cc2cf6ab2feab
davisc0801 <davisc1014@gmail.com>
2019-05-21 09:09:10 -0600

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+Artist.destroy_all
+
+rock = Playlist.create!(name: "Classic Rock")
+country = Playlist.create!(name: "Country")
+jams = Playlist.create!(name: "Favorite Jams")
+
+talking_heads = Artist.create!(name: "Talking Heads")
+journey = Artist.create!(name: "Journey")
+zac_brown = Artist.create!(name: "Zac Brown Band")
+
+place = rock.songs.create!(title: "This Must Be The Place", length: 832, play_count: 83209, artist: talking_heads)
+heaven = rock.songs.create!(title: "Heaven", length: 832, play_count: 83209, artist: talking_heads)
+dont_stop = rock.songs.create!(title: "Don't Stop Believin'", length: 832, play_count: 83209, artist: journey)
+
+chicken = zac_brown.songs.create!(title: "Chicken Fried", length: 4378, play_count: 7453689)
+country.songs << chicken
+jams.songs << chicken
+jams.songs.push(place)
