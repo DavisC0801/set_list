e8ad6d5f28fa8ef5695ed26a3e44ad3c45227113
davisc0801 <davisc1014@gmail.com>
2019-05-21 11:06:38 -0600

    Add defualt user spec for admin categories feature

--- spec/features/admin/categories/index_spec.rb
+++ spec/features/admin/categories/index_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe "User visits categories index page" do
       expect(page).to have_content("Admin Categories")
     end
   end
+
+  context "as a default user" do
+    it "does not allow the user to see admin categories index" do
+      user = User.create(username: "fern@gully.com", password: "password", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_categories_path
+
+      expect(page).to_not have_content("Admin Categories")
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
 end
