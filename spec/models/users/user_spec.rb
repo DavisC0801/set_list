85b2e3f581c75da97d4618dcf817ced1f23434ee
davisc0801 <davisc1014@gmail.com>
2019-05-21 10:15:04 -0600

    Add model tests for roles

--- spec/models/users/user_spec.rb
+++ spec/models/users/user_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:username)}
     it {should validate_presence_of(:password)}
   end
+
+  describe "roles" do
+    it "can be created as an admin" do
+      user = User.create(name: "penelope", password: "boom", role: 1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      user = User.create(name: "sammy", password: "test", role: 0)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+  end
 end
