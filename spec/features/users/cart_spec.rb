0853cd52fff6b8f263c27ce93c0a3d05fd8dc94d
davisc0801 <davisc1014@gmail.com>
2019-05-22 10:14:14 -0600

    Refactor test for before_do and add test for multiple songs in cart

--- spec/features/users/cart_spec.rb
+++ spec/features/users/cart_spec.rb
@@ -1,18 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "As a user" do
+  before :each do
+    artist = Artist.create(name: "Pat The Bunny")
+    @song = artist.songs.create(title: "Song for a Netflix Account", length: 186, play_count: 1000000)
+    @song_2 = artist.songs.create(title: "Frome Here to Utopia", length: 654, play_count: 1500000)
+  end
+
   describe "when I add a song to my cart" do
     it "displays a message" do
-      artist = Artist.create(name: "Pat The Bunny")
-      song = artist.songs.create(title: "Song for a Netflix Account", length: 186, play_count: 1000000)
-
       visit songs_path
 
-      within("#song-#{song.id}-info") do
+      within("#song-#{@song.id}-info") do
         click_button "Add Song"
       end
 
-      expect(page).to have_content("You now have 1 copy of #{song.title} in your cart.")
+      expect(page).to have_content("You now have 1 copy of #{@song.title} in your cart.")
+    end
+  end
+
+  describe "when I add multiple songs to the cart" do
+    it "the message is correct" do
+      visit songs_path
+
+      within("#song-#{@song.id}-info") do
+        click_button "Add Song"
+      end
+
+      within("#song-#{@song_2.id}-info") do
+        click_button "Add Song"
+      end
+
+      within("#song-#{@song.id}-info") do
+        click_button "Add Song"
+      end
+
+      expect(page).to have_content("You now have 2 copies of #{@song.title} in your cart.")
     end
   end
 end
