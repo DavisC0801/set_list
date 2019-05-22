043da1180b665694a424848cc2ccefed6dd4530e
davisc0801 <davisc1014@gmail.com>
2019-05-22 11:14:35 -0600

    Add tests for additional cart model methods

--- spec/models/cart/cart_spec.rb
+++ spec/models/cart/cart_spec.rb
@@ -1,12 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Cart do
+  song_hash = {"1" => 2, "2" => 3)
+  subject { Cart.new cart = Cart.new(song_hash)}
   describe "total_count" do
     it "can calculate a total count" do
-      song_hash = {"1" => 2, "2" => 3}
-      cart = Cart.new(song_hash)
+      expect(subject.total_count).to eq(5)
+    end
+  end
 
-      expect(cart.total_count).to eq(5)
+  describe "add song" do
+    it "adds a song to contents" do
+      subject.add_song(1)
+      subject.add_song(2)
+
+      expected = {"1" => 3, "2" => 4}
+      expect(subject).to eq(expected)
+    end
+  end
+
+  describe "count_of" do
+    it "counts individual songs" do
+      expect(subject.count_of(5)).to eq(0)
+      expect(subject.count_of(1)).to eq(2)
+      expect(subject.count_of(2)).to eq(3)
     end
   end
 end
