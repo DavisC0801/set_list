3bdf677fedb5f4d78ccdface702a4d39004f25f6
davisc0801 <davisc1014@gmail.com>
2019-05-22 09:22:36 -0600

    Update controllers to include private and helper method calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :current_admin?
 
+private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-private
   def current_admin?
     current_user && current_user.admin?
   end
