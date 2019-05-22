8b9436acff887c8eba6ce5685b8a4b2462ae94af
davisc0801 <davisc1014@gmail.com>
2019-05-22 11:26:58 -0600

    Add additional methods to cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,4 +9,12 @@ class Cart
   def total_count
     @contents.values.sum
   end
+
+  def add_song(id)
+    @contents[id.to_s] = count_of(id) + 1
+  end
+
+  def count_of(id)
+    @contents[id.to_s].to_i
+  end
 end
--- spec/models/cart/cart_spec.rb
+++ spec/models/cart/cart_spec.rb
@@ -1,11 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Cart do
-  song_hash = {"1" => 2, "2" => 3)
-  subject { Cart.new cart = Cart.new(song_hash)}
+  song_hash = {"1" => 2, "2" => 3}
+  subject { Cart.new(song_hash) }
   describe "total_count" do
     it "can calculate a total count" do
       expect(subject.total_count).to eq(5)
+    end
+  end
+
+  describe "count_of" do
+    it "counts individual songs" do
+      expect(subject.count_of(5)).to eq(0)
+      expect(subject.count_of(1)).to eq(2)
+      expect(subject.count_of(2)).to eq(3)
     end
   end
 
@@ -15,15 +23,7 @@ RSpec.describe Cart do
       subject.add_song(2)
 
       expected = {"1" => 3, "2" => 4}
-      expect(subject).to eq(expected)
-    end
-  end
-
-  describe "count_of" do
-    it "counts individual songs" do
-      expect(subject.count_of(5)).to eq(0)
-      expect(subject.count_of(1)).to eq(2)
-      expect(subject.count_of(2)).to eq(3)
+      expect(subject.contents).to eq(expected)
     end
   end
 end
